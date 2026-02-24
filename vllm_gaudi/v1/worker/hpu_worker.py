67f747e32a7dff93f0a3924b99329b7eb3e15f44
Tanner Voas <tanner.voas@intel.com>
2026-02-24 11:20:21 +0800

    try again

--- vllm_gaudi/v1/worker/hpu_worker.py
+++ vllm_gaudi/v1/worker/hpu_worker.py
@@ -356,7 +356,7 @@ class HPUWorker(WorkerBase):
                   scheduler_output.scheduled_cached_reqs.req_ids
         
         # Log to CSV
-        csv_path = os.environ.get("VLLM_TIME_LOG_CSV", "vllm_times.csv")
+        csv_path = os.environ.get("VLLM_TIME_LOG_CSV", "/workspace/vllm_times.csv")
         file_exists = os.path.isfile(csv_path)
         with open(csv_path, "a") as f:
             if not file_exists:
